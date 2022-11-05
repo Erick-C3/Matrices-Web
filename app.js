2c727fd18ef2e1bc4e8b526fc5ba57d908cdf76a
Erick <erick.nahuelpaiva.001@gmail.com>
2022-11-04 22:59:03 -0300

    avance viernes

---
+++ app.js
@@ -0,0 +1,56 @@
+
+const matriz = [
+    ["😎","😎","O"],
+    ["😎","X","😎"],
+    ["😎","😎","😎"],
+]
+
+/* document.querySelector("#contenedor-matriz").innerHTML = ` 
+    <div class="row">
+        <div class="col casilla otra-clase">
+            <button type="button" class="btn btn-primary boton-casilla">😎</button>
+        </div>
+    </div>
+`;
+ */
+let matrizHtml = "";
+
+for (let i = 0; i < matriz.length; i++) {
+    matrizHtml += `
+        <div class="row" id="fila-${i}">
+    `;
+    for (let j = 0; j < matriz[i].length; j++) {
+        matrizHtml += `
+            <div class="col casilla otra-clase" id="col-${j}">
+                <button id="btn-${i}-${j}" type="button" class="btn btn-primary boton-casilla">${matriz[i][j]}</button>
+            </div>
+        `;
+    }
+    matrizHtml += `</div>`;
+}
+document.querySelector("#contenedor-matriz").innerHTML = matrizHtml;
+
+for (let i = 0; i < matriz.length; i++) {
+    for (let j = 0; j < matriz[i].length; j++) {
+        console.log( document.querySelector(`#btn-${i}-${j}`).innerHTML);
+    }
+    
+}
+
+
+/* //quiero mostrar el contenido de la casilla
+function alerta(boton){
+    console.log(boton);
+    alert("PRESIONASTE", boton);
+} */
+
+
+/* let i = 0;
+let j = 0;
+matriz.forEach(fila => {
+    matrizHtml += `
+        <div class="row" id="fila${}">
+        </div>
+    `;
+});
+ */
